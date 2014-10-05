3fb463d43ff634b0cf5ba92b4b13b5f0654bf36b
Peter Rabbitson <ribasushi@cpan.org>
2014-10-05 21:22:20 +0000

    Import of RIBASUSHI/Class-Accessor-Grouped-0.10012 from CPAN.
    gitpan-cpan-distribution: Class-Accessor-Grouped
    gitpan-cpan-version:      0.10012
    gitpan-cpan-path:         RIBASUSHI/Class-Accessor-Grouped-0.10012.tar.gz
    gitpan-cpan-author:       RIBASUSHI
    gitpan-cpan-maturity:     released

--- lib/Class/Accessor/Grouped.pm
+++ lib/Class/Accessor/Grouped.pm
@@ -15,7 +15,7 @@ BEGIN {
   }
 }
 
-our $VERSION = '0.10011_01';
+our $VERSION = '0.10012';
 $VERSION = eval $VERSION if $VERSION =~ /_/; # numify for warning-free dev releases
 
 # when changing minimum version don't forget to adjust Makefile.PL as well
